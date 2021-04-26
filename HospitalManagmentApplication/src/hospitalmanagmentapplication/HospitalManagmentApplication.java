8879659e0275c5f8ccead9595764ef810d0bc618
TriangleBear <kakanini31@gmail.com>
2021-04-26 18:23:59 +0800

    Revert "Login Button Not Accessible"
    This reverts commit 285e8ad5841ab88596c64a49f195246d9251c1b0.

--- HospitalManagmentApplication/src/hospitalmanagmentapplication/HospitalManagmentApplication.java
+++ HospitalManagmentApplication/src/hospitalmanagmentapplication/HospitalManagmentApplication.java
@@ -15,8 +15,7 @@ public class HospitalManagmentApplication {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Login lgn = new Login();
-        lgn.setVisible(true);
+        // TODO code application logic here
     }
     
 }
