7ba06ebf5c9351fbb094494da2b1d1ab1882108d
TriangleBear <kakanini31@gmail.com>
2021-04-30 11:00:23 +0800

    Center window

--- loginHospital/src/main/java/userPass/recepLogin.java
+++ loginHospital/src/main/java/userPass/recepLogin.java
@@ -123,6 +123,7 @@ public class recepLogin extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void loginButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginButtonActionPerformed
