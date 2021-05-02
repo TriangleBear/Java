1e546816c19cac6b892788e57b4a8bbc5fefb651
TriangleBear <kakanini31@gmail.com>
2021-05-02 20:09:31 +0800

    Added new ConvertWords

---
+++ ConvertWords/src/main/java/org/zacarias/convertwords/startUp.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.zacarias.convertwords;
+
+/**
+ *
+ * @author ACER
+ */
+public class startUp {
+    public static void main(String[] args) {
+        ConvertForm CF = new ConvertForm();
+        CF.setVisible(true);
+    }
+}
